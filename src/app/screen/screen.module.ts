0deba0495c2ce41d929ac08b2d894f06c93f9581
Salma Samir <salmashoair@gmail.com>
2020-12-13 22:01:05 +0200

    Add files via upload

---
+++ src/app/screen/screen.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+
+
+
+@NgModule({
+  declarations: [SidemenuComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [SidemenuComponent]
+})
+export class ScreenModule { }
